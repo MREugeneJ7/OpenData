e8fb3f50a2305caaa43c57f407b7c2c33b4d2dfa
Usuario <Usuario@192.168.1.104>
2019-01-24 20:51:09 +0000

    Comparing

--- OpenData/src/proyecto.py
+++ OpenData/src/proyecto.py
@@ -5,19 +5,25 @@ Created on 22 ene. 2019
 '''
 
 if __name__ == '__main__':
+    '''
+        Extraer datos de las exportaciones totales por año
+    '''
     with open('exportaciones totales.txt', 'r') as myfile:
         exportaciones = myfile.read()
     exportaciones = exportaciones.split(',')
     exportaciones = exportaciones[13::]
     exportaciones = exportaciones[::-1]
-    expPorAno = [[],[]]
+    expPorAno = [[],[], []]
     expPorAno[0] = [2012,2013,2014,2015,2016,2017]
     expPorAno[1] = exportaciones
     c = 0
     for i in exportaciones:
         expPorAno[1][c] = float(i)
         c += 1
-    print(expPorAno)
+    '''
+        Extraer datos del paro
+    '''
+    expPorAno[2] = [0,0,0,0,0,0]
     with open('paro.tsv', 'r') as myfile:
         paro = myfile.read()
     paro = paro.split('\n')
@@ -38,3 +44,11 @@ if __name__ == '__main__':
     for a in expPorAno[2]:
         a = a/12
     print(expPorAno)
+    
+    comparative = [[],[]]
+    comparative[0] = expPorAno[0]
+    comparative[1] = ['']*6
+    for i in range(0,6):
+        comparative[1][i] = expPorAno[2][i]/expPorAno[1][i]
+    
+    print(comparative)
